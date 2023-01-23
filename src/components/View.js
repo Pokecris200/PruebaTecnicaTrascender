940b3f33fe080d0b437c554091ead97d8e90a6be
Cristian Forero <cristian.forero-m@mail.escuelaing.edu.co>
2023-01-22 23:41:25 -0500

    avances a nivel Visual

--- src/components/View.js
+++ src/components/View.js
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from 'react';
+import {useState} from 'react';
 
 function View() {
+  const [pok, setPok] = useState("MissigNo") ;
   return (
     <div className = "card">
-        <img src="https://m.media-amazon.com/images/I/61Y8VHcrPXL.jpg" alt=""/>
+        <img src="https://img.pokemondb.net/sprites/home/normal/turtwig.png" alt="" />
         <div className = "card-body">
-            <h4 className = "card-title">My title</h4>
+            <h4 className = "card-title">{pok}</h4>
             <p className = "card-text text-secondary">asdfghjklsdfghjkldfghjm</p>
         </div>
     </div>
--- src/components/ViewContainer.js
+++ src/components/ViewContainer.js
@@ -1,14 +1,26 @@
 import React from 'react'
 import View from './View'
+import {Vtest} from './View.js'
+
+function test(variable) {
+    
+}
 
 function ViewContainer() {
   return (
-    <div className="container d-flex justify-content-center">
+    <div className="container d-flex justify-content-center align-items-center h-100">
         <div className="row">
-            <div className="col-md-4">
+            <div className="col-md-6">
                 <View/>
-                <View/>
-                <View/>
+            </div>
+            <div className="col-md-6">
+                <form onSubmit= {ev => {
+                    ev.preventDefault();
+                    test(ev.target.search.value);
+                    }}>
+                    <input type="text" name="search" autoComplete="off"></input>
+                    <button type="submit">buscar</button>
+                </form> 
             </div>
         </div>
     </div>
