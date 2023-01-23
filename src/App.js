37b135c77d27eb5b3c98dcb7b87d99b83474f535
Cristian Forero <cristian.forero-m@mail.escuelaing.edu.co>
2023-01-23 10:27:31 -0500

    Corrección errores

--- src/App.js
+++ src/App.js
@@ -8,11 +8,10 @@ import { useState, useEffect } from "react";
 mongoose.connect('mongodb://127.0.0.1:27017/test');*/
 
 function App() {
-  const [pokemons, setPokemons] = useState([]);
   
   fetch("https://pokeapi.co/api/v2/pokemon/1/")
     .then(data => {return data.json()})
-    .then(res => console.log(res);)
+    .then(res => console.log(res))
 
   return (
     <div className="App">
--- src/components/View.js
+++ src/components/View.js
@@ -1,14 +1,11 @@
 import React from 'react';
-import {useState} from 'react';
 
 function View() {
-  const [pok, setPok] = useState("MissigNo") ;
-  setPok("MissigNo");
   return (
     <div className = "card">
         <img src="https://img.pokemondb.net/sprites/home/normal/turtwig.png" alt="" />
         <div className = "card-body">
-            <h4 className = "card-title">{pok}</h4>
+            <h4 className = "card-title">MissigNo</h4>
             <p className = "card-text text-secondary">asdfghjklsdfghjkldfghjm</p>
         </div>
     </div>
